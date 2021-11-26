781539de5bf009c3a4e3f7774dffea8d22e500bc
imtheunix <49956370+imtheunix@users.noreply.github.com>
2021-11-26 19:47:11 -0300

    Update settings.py

--- notas/settings.py
+++ notas/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xl+#7g494es-0jx7pio+e$p4n7ltb0dzcc(5cv4g0=u0v&si-0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['http://www.testeapidjango.herokuapp.com']
+ALLOWED_HOSTS = ['https://testeapidjango.herokuapp.com/']
 
 
 # Application definition
