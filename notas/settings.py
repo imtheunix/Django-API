8c1fed23be6b682d063059420303bd86c2bf281d
imtheunix <49956370+imtheunix@users.noreply.github.com>
2021-11-30 15:35:39 -0300

    Update settings.py

--- notas/settings.py
+++ notas/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xl+#7g494es-0jx7pio+e$p4n7ltb0dzcc(5cv4g0=u0v&si-0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".herokuapp.com", "127.0.0.1"]
 
